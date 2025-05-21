adc1d94da580ac518f11353e2f5bd2c2ccab2aeb
Avinash Singh Munda <96285074+avimunda777@users.noreply.github.com>
2025-05-21 20:32:21 +0530

    Add files via upload

---
+++ Implicit.java
@@ -0,0 +1,10 @@
+                    // Implicit TypeCasting
+class implicit
+{
+    public static void main(String[] args)
+    {
+        int a = 10; // 4 Bytes
+        double b = a; // 8 Bytes
+        System.out.println(b);
+    }
+} 
